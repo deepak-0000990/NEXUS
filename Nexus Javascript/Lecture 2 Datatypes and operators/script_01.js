772d2901f827bcc4de8a4c2588e457d9dee4898f
Deepak kumar panigrahi <panigrahideepakkumar08@gmail.com>
2026-07-14 21:47:25 +0530

    Add files via upload

---
+++ Nexus Javascript/Lecture 2 Datatypes and operators/script_01.js
@@ -0,0 +1,102 @@
+// declare the variable 
+// let num = 10;
+// console.log(num);
+// primitive datatype
+
+// Number,Boolean,string,undefined,null,symbol,bigint
+
+//number
+// let account_balance = 20;
+// console.log(typeof account_balance);
+// //string
+// let str=("Deepak")
+// console.log(str)
+// // Boolean
+// let statement = true
+// console.log(statement)
+// //undefined
+// let account
+// console.log(account)
+// // null
+// let bal = null
+// console.log(bal)
+// let a = 123456789034897394934693659635963957307430801764389405665709n
+// console.log(a)
+
+
+
+//non primitive datatype
+//Array,Object,Function
+
+// let Array = [10,30,50]
+// console.log(Array)
+
+// //objec
+// let obj = {
+//     name:"Deepak",
+//     account_number:4567890987,
+//     account_balance:900,
+// }
+
+// console.log(obj)
+
+// let fun = function(){
+//     console.log("Deepak")
+//     return 10
+// }
+// fun()
+
+// Type conversion
+// let account_balance="20"
+// let num = Number(account_balance)
+// console.log(typeof account_balance)
+// console.log(num)
+
+// Boolean convert to number
+// let x = true
+// console.log(Number(x))
+// let account = "100xs"
+// console.log(Number(account))
+
+// null
+// let x1 = null 
+// console.log(Number(x1))
+
+// undefined
+// let x2
+// console.log(Number(x2))
+
+// string convert into0 number
+// let ab = 20
+// console.log(String(ab))
+
+// let x = false
+// console.log(String(x))
+
+// Boolean
+// let abc = ""
+// console.log(Boolean(abc))
+
+// console.log(((6*(3+18)/6)-9))
+// Divide Multiply Left to Right
+// Add sub Left to Right
+
+// Modulous Give reminder
+// console.log(20%3)
+// // ++increment operator ,--decremtn oprator 
+// let sum = 20
+//--sum 
+// sum++ post increment , sum-- post decremnt
+// ++sum pre increment , --sum pre decrement 
+// let total = ++sum 
+// console.log(total)
+// console.log(sum)
+
+// assignment operator 
+// let x = 20
+// x+=10 
+// // x = x+10
+// console.log(x)
+
+// let x = 20 
+// console.log(--x)
---
+++ Nexus Javascript/Lecture 3Comaprison operator/script_3.js
@@ -0,0 +1,53 @@
+// comparison operator 
+// number to number
+
+// let a1 = 1 
+// let a2 = 2
+// console.log(a1==a2)
+// == both are equal 
+// <= less thaan equal to , >= greater than equal to 
+
+// let num = 10 
+// let str1 = "10"
+// console.log(num==str1)
+//Type conversion hoga string to number 
+
+// let a3 = 10 
+// let str2= "10"
+// console.log(a3==str2)
+// // === type check,then compare the value 
+// console.log(a3===str1)
+
+// undefine comparison
+null == undefined //true
+null === undefined //false
+
+// console.log(null == undefined)
+// console.log(null === undefined)
+// null can be equivalent to undefined ==
+console.log(null==0)
+console.log(null<0)
+console.log(null>0)
+console.log(null<=0)
+console.log(null>=0)
+
+// console.log(NaN==NaN)
+// let str3 ="Deepak"
+// let str4 = "mohan"
+// console.log(Number(str3))
+
+// let abc1 = 123
+// let abc2 = "123"
+// let abc3= 123
+// console.log(abc1==abc2==abc3)
+
+// console.log(undefined!==null)
+
+// console.log(undefined==0)
+// console.log(undefined<0)
+// console.log(undefined>0)
+// console.log(undefined<=0)
+// console.log(undefined>=0)
+
+
+// console.log(null==0)
